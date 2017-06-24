15cedae4c427559a46650b6275a9eec4fbbd753c
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-06-24 12:33:12 +0200

    init Spotify integration

---
+++ backend/config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Vadym Yatsyuk on 24.06.17
+ */
+
+module.exports = {
+  spotify: {
+    CLIENT_ID: 'aceb81be23b746b9a2f911d2808cc295',
+    CLIENT_SECRET: '797405dc59964a2e928821112e488ca1'
+  }
+};
--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,15 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const SpotifyWebApi = require('spotify-web-api-node');
 
+const config = require('./config');
 const PORT = 3000;
+var spotifyApi = new SpotifyWebApi({
+  clientId : config.CLIENT_ID,
+  clientSecret : config.CLIENT_SECRET,
+  redirectUri : `http://localhost:${ PORT }/callback`
+});
 
 server.listen(PORT, () => {
   console.log(`Listening on port ${ PORT }`);
