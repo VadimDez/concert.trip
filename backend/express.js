a56c59eca280124049bb5be6ff2bb2d9d782a570
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-06-24 17:12:16 +0200

    enabled cors

--- backend/express.js
+++ backend/express.js
@@ -17,11 +17,23 @@ const config = require('./config');
 
 module.exports = (app) => {
   passport.serializeUser(function(user, done) {
+    console.log('======');
+    console.log('user');
+    console.log(user);
     done(null, user);
   });
 
   passport.deserializeUser(function(obj, done) {
+    console.log('======');
+    console.log('obj');
+    console.log(obj);
     done(null, obj);
+  });
+
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
   });
 
   app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,25 +41,26 @@ module.exports = (app) => {
   app.use(methodOverride());
   app.use(cookieParser());
   app.use(passport.initialize());
-  passport.use(new SpotifyStrategy.Strategy({
+  passport.use(new SpotifyStrategy.Strategy(
+    {
       clientID: config.spotify.CLIENT_ID,
       clientSecret: config.spotify.CLIENT_SECRET,
       callbackURL: `http://localhost:${ 3000 }/auth/spotify/callback`
     },
     function(accessToken, refreshToken, profile, done) {
-      User.findOrCreate({ spotifyId: profile.id }, function (err, user) {
-        return done(err, user);
-      });
+      // User.findOrCreate({ spotifyId: profile.id }, function (err, user) {
+        return done(null, profile);
+      // });
     }
   ));
   app.use(session({
     secret: config.secrets.session,
     saveUninitialized: true,
     resave: false,
-    store: new mongoStore({
-      mongooseConnection: mongoose.connection,
-      db: 'concert-trip'
-    })
+    // store: new mongoStore({
+    //   mongooseConnection: mongoose.connection,
+    //   db: 'concert-trip'
+    // })
   }));
 
 
