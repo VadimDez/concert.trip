cb0c147af636203bc46d8f53aaeba4a90b23f280
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-06-24 20:20:54 +0200

    get transport

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ mongoose.Promise = Promise;
 Promise.promisifyAll(mongoose);
 const chalk = require('chalk');
 const passport = require('passport');
+const request = require('request');
 
 const User = require('./models/user');
 const authService = require('./auth.service');
@@ -48,6 +49,58 @@ app.get('/spotify/topArtists', ensureAuthenticated, (req, res) => {
   }, function(err) {
     console.error(err);
   });
+});
+
+/**
+ * Get transport prices, routes, etc...
+ *
+ * @param originCity - Origin display name
+ * @param originPosition - Origin latitude,longitude (comma separated)
+ * @param destinationCity - Destination display name
+ * @param destinationPosition - Destination latitude,longitude (comma separated)
+ */
+function getTransport(originCity, originPosition, destinationCity, destinationPosition, currency) {
+  currency = currency || 'EUR';
+  let url = `https://rome2rio12.p.mashape.com/Search?currency=${ currency }&dKind=City&oKind=City`;
+  const args = ['oName', 'oPos', 'dName', 'dPos'];
+  originCity = encodeURIComponent(originCity) || null;
+  destinationCity = encodeURIComponent(destinationCity) || null;
+
+  // add query params dynamically
+  args.forEach((value, index) => {
+    if (arguments[index]) {
+      url += `&${ value }=${ arguments[index] }`;
+    }
+  });
+
+  const options = {
+    url: url,
+    headers: {
+      'X-Mashape-Key': 'popMmKAzdQmshoMEn4fhbUmGcGxDp1SbSidjsn3U3dEDruOfp1',
+      'Accept': 'application/json'
+    }
+  };
+
+  return new Promise((resolve, reject) => {
+    request(options, (error, response, body) => {
+      if (!error && response.statusCode === 200) {
+        const json = JSON.parse(body);
+        resolve(json);
+      } else {
+        reject();
+      }
+    });
+  });
+}
+
+app.get('/transport', (req, res) => {
+  getTransport('Zurich', null, 'Munich')
+    .then((response) => {
+      res.status(200).json(response).end();
+    })
+    .catch(() => {
+      res.status(400).end();
+    });
 });
 
 server.listen(PORT, () => {
