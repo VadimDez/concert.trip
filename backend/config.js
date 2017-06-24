8314bbcd3ab04943d73269932dde5719b9666d2a
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-06-24 12:50:02 +0200

    added mongodb

--- backend/config.js
+++ backend/config.js
@@ -2,9 +2,14 @@
  * Created by Vadym Yatsyuk on 24.06.17
  */
 
+// @TODO will be changed before presentation.
+const user = 'admin';
+const password = 'admin1';
+
 module.exports = {
   spotify: {
     CLIENT_ID: 'aceb81be23b746b9a2f911d2808cc295',
     CLIENT_SECRET: '797405dc59964a2e928821112e488ca1'
-  }
+  },
+  MONGODB: `mongodb://${ user }:${ password }@ds129462.mlab.com:29462/concert-trip`
 };
--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,20 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const SpotifyWebApi = require('spotify-web-api-node');
+const mongoose = require('mongoose');
+const chalk = require('chalk');
 
 const config = require('./config');
 const PORT = 3000;
-var spotifyApi = new SpotifyWebApi({
+const spotifyApi = new SpotifyWebApi({
   clientId : config.CLIENT_ID,
   clientSecret : config.CLIENT_SECRET,
   redirectUri : `http://localhost:${ PORT }/callback`
+});
+mongoose.connect(config.MONGODB);
+mongoose.connection.on('error', err => {
+  console.log(chalk.red('MongoDB error: '));
+  console.log(chalk.red(err));
 });
 
 server.listen(PORT, () => {
