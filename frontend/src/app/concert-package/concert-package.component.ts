ff2c21f0548f5b2c00f0c38158d325a89e710046
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2017-06-25 15:13:46 +0200

    get price

--- frontend/src/app/concert-package/concert-package.component.ts
+++ frontend/src/app/concert-package/concert-package.component.ts
@@ -6,11 +6,22 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./concert-package.component.css']
 })
 export class ConcertPackageComponent {
-  @Input() concertData: Object;
+  @Input() concertData: any;
   constructor() { }
 
   getPrice() {
     console.log(this.concertData);
-    // this.concertData
+
+    return this.concertData.concert.price
+      + parseInt(this.getBookingPrice(), 10)
+      + parseInt(this.getTransportPrice(), 10);
+  }
+
+  getTransportPrice() {
+    return this.concertData.transport && this.concertData.transport.indicativePrice ? this.concertData.transport.indicativePrice.price : 0
+  }
+
+  getBookingPrice() {
+    return this.concertData.booking && this.concertData.booking.hotels && this.concertData.booking.hotels.length ? this.concertData.booking.hotels[0].price : 0;
   }
 }
