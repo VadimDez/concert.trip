667f18d8e577e27d07d467e390f53e42e9ece53d
Paulius Šukys <paul.sukys@gmail.com>
2017-06-25 11:44:49 +0200

    api artist concerts route implementation

--- backend/api.js
+++ backend/api.js
@@ -63,61 +63,34 @@ function getArtists() {
     });
 }
 
-function getConcerts() {
-  return spotifyApi.getMyTopArtists().then(spotify_artists => {
-    return Promise.all(spotify_artists.body.items.map(spotify_artist => {
-      return eventfulApi.search_performers({ keywords: spotify_artist.name }).then(performers => {
-        if (performers.total_items != 0 && performers.performers && performers.performers.performer) {
-          let artist = null;
-          if (performers.total_items == 1) {
-            artist = performers.performers.performer;
-          } else {
-            artist = performers.performers.performer[0];
-          }
-          return eventfulApi.get_performer_events({ id: artist.id }).then(performer_events => {
-            if (performer_events.event_count != 0) {
-              // start building offer from here (has events)
-              let artist_offer = { name: spotify_artist.name, offers: [] };
-              return Promise.all(performer_events.event.map(p_event => {
-                return eventfulApi.get_event({ id: p_event.id }).then(event_details => {
-                  if (event_details.price) {
-                    let default_end_time = new Date(event_details.start_time);
-                    default_end_time.setHours(default_end_time.getHours() + 8);
-                    let event_offer = {
-                      trip: {},
-                      accommodation: {},
-                      concert: {
-                        title: event_details.title,
-                        venue_name: event_details.venue_name,
-                        address: event_details.address,
-                        city: event_details.city,
-                        country: event_details.country,
-                        start_time: event_details.start_time,
-                        end_time: event_details.end_time || default_end_time,
-                        price: eventfulApi.parsePrice(event_details.price),
-                        tickets_url: eventfulApi.getTicketURL(event_details)
-                      }
-                    };
-                    return event_offer;
-                  }
-                });
-              })).then(p_events => {
-                return p_events.filter(p_event => {
-                  return p_event !== undefined;
-                });
-              }).then(event_offers => {
-                artist_offer.offers = event_offers;
-                return artist_offer;
-              });
+function getConcerts(artist_id) {
+  return eventfulApi.get_performer_events({ id: artist_id }).then(artist_events => {
+    if (artist_events.event_count != 0) {
+      return Promise.all(artist_events.event.map(p_event => {
+        return eventfulApi.get_event({ id: p_event.id }).then(event_details => {
+          if (event_details.price) {
+            let default_end_time = new Date(event_details.start_time);
+            default_end_time.setHours(default_end_time.getHours() + 8);
+            let concerts = {
+              title: event_details.title,
+              venue_name: event_details.venue_name,
+              address: event_details.address,
+              city: event_details.city,
+              country: event_details.country,
+              start_time: event_details.start_time,
+              end_time: event_details.end_time || default_end_time,
+              price: eventfulApi.parsePrice(event_details.price),
+              tickets_url: eventfulApi.getTicketURL(event_details)
             }
-          });
-        }
-      });
-    })).then(offers => {
-      return offers.filter(offer => {
-        return offer !== undefined;
-      });
-    });
+            return concerts;
+          }
+        });
+      })).then(p_events => {
+        return p_events.filter(p_event => {
+          return p_event !== undefined;
+        })
+      })
+    }
   });
 }
 
@@ -135,7 +108,12 @@ router.get('/api/artists', ensureAuthenticated, (req, res) => {
 
 // return estimated price for bundle = concert + transport + booking
 router.get('/api/artists/:id/concerts', (req, res) => {
-  res.json([]).end();
+  getConcerts(req.params.id).then(concerts => {
+    res.status(200).json(concerts).end();
+  }).catch(err => {
+    console.log(chalk.red(err));
+    res.status(400).end();
+  })
 });
 
 router.get('/transport', (req, res) => {
